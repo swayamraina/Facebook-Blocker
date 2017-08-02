38084740e925ef069e537b9c391c84641b0594de
Swayam Raina <code.monk.wi@gmail.com>
2017-08-03 00:25:22 +0530

    javascsript file to be injected when user opens facebook tab

---
+++ facebook-blocker.js
@@ -0,0 +1,6 @@
+// hide everything from facebook
+document.getElementsByTagName('body')[0].remove();
+
+// add empty body element
+// TODO : add ne wfeatures here
+document.body = document.createElement("body");
