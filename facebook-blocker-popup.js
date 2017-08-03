35818a288e8e3648795f1c38fd0d3fd98827094f
Swayam Raina <code.monk.wi@gmail.com>
2017-08-03 23:47:18 +0530

    display time left in popup

--- facebook-blocker-popup.js
+++ facebook-blocker-popup.js
@@ -2,6 +2,14 @@ function updateDisplay() {
 	document.getElementById('timer')[0].innerHTML = getFromDB("minutes") + " : " + getFromDB("seconds");
 }
 
+var timer = setInterval(function(){
+	updateDisplay();
+}, 1000);
+
+function updateDisplay() {
+	document.getElementById('timer').innerHTML = getFromDB("minutes") + " : " + getFromDB("seconds");
+}
+
 function getFromDB(key) {
 	return localStorage.getItem(key);
 }
