1750853e81d064ba12ac0d9f46fe0bc6dec82a81
Swayam Raina <code.monk.wi@gmail.com>
2017-08-05 16:51:39 +0530

    Updated popup js

--- facebook-blocker-popup.js
+++ facebook-blocker-popup.js
@@ -1,15 +1,56 @@
-function updateDisplay() {
-	document.getElementById('timer')[0].innerHTML = getFromDB("minutes") + " : " + getFromDB("seconds");
-}
+var minutes = loadSynchronizedMinutesValue();
+var seconds = loadSynchronizedSecondsValue();
 
-var timer = setInterval(function(){
+var timer = setInterval(function() {
+	updateTime();
 	updateDisplay();
+	updateTimer();
 }, 1000);
 
+function updateTime() {
+	updateSeconds();
+	updateMinutes();
+}
+
+function updateSeconds() {
+	if(seconds==0) {
+		seconds = 60;
+	}
+	seconds = (seconds-1)%60;
+	saveToDB("seconds", seconds);
+	saveToDB("lastUpdated", new Date());
+}
+
+function updateMinutes() {
+	if(seconds==0 && minutes>0) {
+		minutes = minutes-1;
+		saveToDB("minutes", minutes);
+	}
+}
+
 function updateDisplay() {
-	document.getElementById('timer').innerHTML = getFromDB("minutes") + " : " + getFromDB("seconds");
+	if(getFromDB("facebook-blocker")) {
+		document.getElementById('timer').innerHTML = "Enough of facebook today!";
+	} else {
+		document.getElementById('timer').innerHTML = getTimeForDisplay(minutes) + " : " + getTimeForDisplay(seconds);
+	}
+}
+
+function getTimeForDisplay(data) {
+	return (data < 10 ? "0"+data : data);
+}
+
+function updateTimer() {
+	if(seconds==0 && minutes==0) {
+		saveToDB("facebook-blocker", true);
+		clearInterval(timer);
+	}
 }
 
 function getFromDB(key) {
 	return localStorage.getItem(key);
+}
+
+function saveToDB(key, value) {
+	localStorage.setItem(key, value);
 }
